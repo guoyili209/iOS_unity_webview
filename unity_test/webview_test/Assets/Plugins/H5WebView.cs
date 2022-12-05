3a5febc087b11ef956fbdecdd8731a9471a118d7
guoyili <guoyili@wudi360.com>
2022-12-05 18:18:30 +0800

    update

--- unity_test/webview_test/Assets/Plugins/H5WebView.cs
+++ unity_test/webview_test/Assets/Plugins/H5WebView.cs
@@ -14,27 +14,37 @@ public class H5WebView : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
-    public void OnButtonClick(){
+    public void OnButtonClick()
+    {
         JSONTest obj = new JSONTest();
         obj.url = "http://192.168.11.153:3000/index.html";
         obj.gameObjName = "OC";
         obj.OCMessageMethodName = "OCMessage";
         // obj.url = "http://www.baidu.com";
-            obj.data = "test";
-            obj.msgType = 100;
-            obj.server_key = "oTSZTWFrC7LJ2HrRFtTgyR2yQo0sK0hi";
-            obj.bundle_id = "com.bingo.match.money.win.sm";
-            obj.udid = "bingo_android_sm_b747f0bd07db0071f3737d1b13f85f4ae1e3978e";
-            obj.authorization = "2310150_wH8FplV6psAcXabkZginMRBeDDkT5Qz93S3v2QiXRPdi25DCEZLYeeN0cXaGhEMI3TEUOV7jR65nmDFfgnUt2hpyWUqYeUfxjEgU";
-            obj.version = 30;
-            obj.timezone="America/Los_Angeles";
+        ADJson adjson = new ADJson();
+        adjson.ad_status = "REWARD";
+        // obj.data = "{\"ad_status\":\"REWARD\",\"network_worth\":\"\",\"network_id\":\"\",\"network_name\":\"\",\"ad_pos\":\"\"}";
+        ChargeJSON chargeJSON = new ChargeJSON();
+        ChargeData chargeData = new ChargeData();
+        chargeData.charge_id = 11017;
+        chargeJSON.data = chargeData;
+        obj.data = chargeJSON;
+        obj.msgType = 101;
+        obj.server_key = "";
+        obj.bundle_id = "com.test.bingocash";
+        obj.udid = "19BEF575-9757-42C8-909B-D3B6CD06CB9B";
+        obj.authorization = "30003918_dc957d71c8b605958415352c31fe3023a118e1a8";
+        obj.version = 44;
+        obj.timezone = "Asia/Shanghai";
+
 
         string json = JsonConvert.SerializeObject(obj);
+        Debug.Log(json);
         CSharpMessage(json);
         // IOSshowWebView("http://www.baidu.com",1);
-        Debug.Log(json);
+
     }
     // Update is called once per frame
     void Update()
@@ -42,19 +52,32 @@ public class H5WebView : MonoBehaviour
 
     }
 }
+public class ChargeData
+{
+    public int charge_id { get; set; }
+}
+public class ChargeJSON
+{
+    public ChargeData data { set; get; }
+}
+public class ADJson
+{
+    public string ad_status { get; set; }
+}
 // [System.Serializable]
-public class JSONTest{
-       public string url{get;set;}
-   public int msgType{get;set;}
-   public string gameObjName{get;set;}
-   public string OCMessageMethodName{get;set;}
-   public string data{get;set;}
-   public string server_key{get;set;}
-   public string bundle_id{get;set;}
+public class JSONTest
+{
+    public string url { get; set; }
+    public int msgType { get; set; }
+    public string gameObjName { get; set; }
+    public string OCMessageMethodName { get; set; }
+    public ChargeJSON data { get; set; }
+    public string server_key { get; set; }
+    public string bundle_id { get; set; }
 
-   public string udid{get;set;}
-   public string authorization{get;set;}
-   public int version{get;set;}
-   public string timezone{get;set;}
+    public string udid { get; set; }
+    public string authorization { get; set; }
+    public int version { get; set; }
+    public string timezone { get; set; }
 
 }
